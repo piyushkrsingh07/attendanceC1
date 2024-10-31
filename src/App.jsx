5c9ac5599301b9c5a2eb37a803779cc24806b7c5
piyushkrsingh07 <piyush23153101@akgec.ac.in>
2024-10-31 12:34:16 +0530

    Dasboard Added By Anjali

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,10 @@ import Home from "./pages/Home"
 import Login from "./pages/Login"
 import Signup from "./pages/Signup"
 import Dashboard from "./pages/Dashboard"
+import Sidebar from "./components/Sidebar"
+import Attendance from './components/Attendance'
+import Timetable from './components/Timetable'
+import Profile from './components/Profile'
 
 import {Route,Routes} from "react-router-dom"
 
@@ -25,7 +29,12 @@ function App() {
         <Route path="/" element={<Home/>} />
         <Route path="/login" element={<Login setIsLoggedIn={setIsLoggedIn}/>} />
         <Route path="/signup" element={<Signup setIsLoggedIn={setIsLoggedIn}/>} />
-        <Route path="/dashboard" element={<Dashboard/>} />
+        <Route path="/dashboard" element={<Dashboard setIsLoggedIn={setIsLoggedIn}/>} />
+
+              <Route path="/Attendance" element={<Attendance setIsLoggedIn={setIsLoggedIn}/>} />
+              <Route path="/Timetable" element={<Timetable setIsLoggedIn={setIsLoggedIn}/>} />
+              <Route path="/Profile" element={<Profile setIsLoggedIn={setIsLoggedIn}/>} />
+
 
 
       </Routes>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { NavLink,Link } from 'react-router-dom'
 import { ToastContainer,toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 import { IoHome } from "react-icons/io5";
+import Sidebar from './Sidebar';
 const Navbar = (props) => {
 
     let IsLoggedIn=props.IsLoggedIn;
     let setIsLoggedIn=props.setIsLoggedIn;
 
   return (
+    <>
+      { !IsLoggedIn &&
     <nav className='relative flex justify-between space-x-3 bg-black h-[50px] mt-0 pl-2 pr-3 items-center w-full'>
       <div className='flex'>
       <NavLink to="/">
@@ -45,13 +48,13 @@ const Navbar = (props) => {
 
         { IsLoggedIn &&
         <NavLink to="/">
-            <button onClick={()=>{
-                setIsLoggedIn(false)
-                toast.success("You are logged out");
+        <button onClick={()=>{
+               setIsLoggedIn(false)
+               toast.success("You are logged out");
                 
-            }} className='text-white bg-gray-500 px-2 py-1 rounded-sm border-hidden transition-all duration:200 hover:border-solid hover:border-white hover:border'>Logout</button>
+         }} className='text-white bg-gray-500 px-2 py-1 rounded-sm border-hidden transition-all duration:200 hover:border-solid hover:border-white hover:border'>Logout</button>
         </NavLink>
-        }
+         }
 
 
 
@@ -67,6 +70,39 @@ const Navbar = (props) => {
 
      
     </nav>
+
+      }
+
+      { IsLoggedIn && 
+         
+         <nav className="bg-black p-3 flex justify-between items-center">
+          <div className='flex gap-x-2 '>
+            <Sidebar/>
+         <div className="text-white text-2xl font-bold mt-4">MarkMe</div>
+         </div>
+         <ul className="md:flex space-x-4">
+           <li><Link to="/dashboard" className="text-white text-xl block px-4 py-2">Dashboard</Link></li>
+           <li><Link to="/Attendance" className="text-white  text-xl block px-4 py-2">Attendance</Link></li>
+           <li><Link to="/Timetable" className="text-white text-xl block px-4 py-2">Timetable</Link></li>
+           <li><Link to="/Profile" className="text-white  text-xl block px-4 py-2">Profile</Link></li>
+
+         </ul>
+         <NavLink to="/">
+        <button onClick={()=>{
+               setIsLoggedIn(false)
+               toast.success("You are logged out");
+                
+         }} className='text-white bg-gray-500 px-2 py-1 rounded-sm border-hidden transition-all duration:200 hover:border-solid hover:border-white hover:border'>Logout</button>
+        </NavLink>
+       </nav>
+
+
+
+
+
+
+      }
+      </>
   )
 }
 
